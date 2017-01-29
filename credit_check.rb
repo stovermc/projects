4f33e6abcc9317ab1ceda80a3e80fd8f441c0c5e
stovermc <stovermc@gmail.com>
2017-01-29 16:12:42 -0700

    Finished Credit Check

--- credit_check.rb
+++ credit_check.rb
@@ -1,32 +1,88 @@
 # Author: Mark Stover
 # Project: Credit check
-# Use program to check if account number is valid or not
+# Use CreditCheck class to check if an account number is valid or not. Pass
+# CreditCheck an account number of any size.
 
-card_number = "342801633855673"
+class CreditCheck
+  attr_reader :acct_number
+  def initialize(acct_number)
+    @acct_number = acct_number
+    create_processing_number
+    double_odds
+    final_collection
+    sum_collection
+    valid?
+  end
 
+  def create_processing_number
+    @processing_number = @acct_number.reverse.split("")
+  end
 
-full_number = card_number.reverse.split("")
+  def double_odds
+    @double_odds = @processing_number.map.with_index do |value, index|
+      if index.odd?
+        value.to_i * 2
+      else
+        value.to_i
+      end
+    end
+  end
 
-doubles = full_number.map.with_index do |value, index|
-  if index.odd?
-    value.to_i * 2
-  else
-    value.to_i
+  def final_collection
+    @final_collection = @double_odds.map{ |number| number > 9 ? number - 9: number }
+  end
+
+  def sum_collection
+    @sum_collection = @final_collection.inject(0,:+)
+  end
+
+  def valid?
+    @sum_collection % 10 == 0
   end
 end
 
-final_collection = doubles.map { |number| number > 9 ? number - 9: number }
+credit_check_1 = CreditCheck.new("342804633855673")
+credit_check_1.create_processing_number
+credit_check_1.double_odds
+credit_check_1.final_collection
+credit_check_1.sum_collection
 
-sum = final_collection.inject(0, :+)
-
-if sum % 10 == 0
+if credit_check_1.valid?
   puts "The number is valid!"
 else
   puts "The number is invalid!"
 end
 
-if sum % 10 == 0
-  puts "The number is valid!"
-else
-  puts "The number is invalid!"
-end
+
+# card_number = "342804633855673"
+# create_processing_number
+# double_odds
+# final_collection
+
+
+
+# full_number = card_number.reverse.split("")
+#
+# doubles = full_number.map.with_index do |value, index|
+#   if index.odd?
+#     value.to_i * 2
+#   else
+#     value.to_i
+#   end
+# end
+#
+# final_collection = doubles.map { |number| number > 9 ? number - 9: number }
+#
+# sum = final_collection.inject(0, :+)
+#
+# if sum % 10 == 0
+#   puts "The number is valid!"
+# else
+#   puts "The number is invalid!"
+# end
+#
+# if sum % 10 == 0
+#   puts "The number is valid!"
+# else
+#   puts "The number is invalid!"
+# end
